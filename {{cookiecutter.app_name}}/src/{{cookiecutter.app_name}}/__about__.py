a329b168c4213e2b6e5593c170faed98e2375646
gjcooper <gjcooper@gmail.com>
2017-11-03 17:03:12 +1100

    Move to newer directory structure and system

---
+++ {{cookiecutter.app_name}}/src/{{cookiecutter.app_name}}/__about__.py
@@ -0,0 +1,14 @@
+
+__version__ = '{{cookiecutter.version}}'
+
+__title__ = '{{cookiecutter.app_name}}'
+
+__summary__ = '{{cookiecutter.project_short_description}}'
+
+__uri__ = 'https://github.com/{{cookiecutter.github_username}}/{{cookiecutter.app_name}}'
+
+__license__ = '{{cookiecutter.license}}'
+
+__author__ = '{{cookiecutter.full_name}}'
+
+__email__ = '{{cookiecutter.email}}'
